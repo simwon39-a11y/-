1cd3ecf18ee7ded62f81a915096b2abf7c14af57
simwon39-a11y <simwon39@gmail.com>
2026-03-09 18:05:40 +0900

    Small SW tweak for better mobile badging compatibility

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
-// Version: 26.03.09.1755
+// Version: 26.03.09.1810
 self.addEventListener('push', function (event) {
+
     const data = event.data.json();
     const options = {
         body: data.body,
